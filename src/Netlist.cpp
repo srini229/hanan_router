c626907c22b94ba9cd26706cbc91b7f06faa23a8
Ramprasath <73570678+srini229@users.noreply.github.com>
2026-02-02 23:13:21 +0530

    updates

--- src/Netlist.cpp
+++ src/Netlist.cpp
@@ -79,6 +79,7 @@ Netlist::Netlist(const std::string& plfile, const::std::string& leffile, const D
         if (params != m.end()) {
           for (auto& p : *params) {
             modu->addPin(p);
+            COUT << p << '\n';
           }
         }
         for (auto& g : globalNets) {
@@ -102,12 +103,15 @@ Netlist::Netlist(const std::string& plfile, const::std::string& leffile, const D
             Placement::Instance* instptr{nullptr};
             if (iname != inst.end() && mname != inst.end()) {
               instptr = modu->addInstance(*iname, *mname, tr);
+              COUT << *iname << ' ' << *mname << '\n';
             }
             if (instptr) {
               auto famap = inst.find("fa_map");
               if (famap != inst.end()) {
                 for (auto& pm : *famap) {
                   const Net* n = &(modu->addNet(pm["actual"]));
+                  COUT << "net : " << pm["actual"] << '\n';
+                  COUT << "pin : " << pm["formal"] << '\n';
                   modu->addTmpPin(n, instptr, pm["formal"]);
                 }
               }
@@ -260,6 +264,10 @@ void Netlist::loadLEF(const std::string& leffile, const DRC::LayerInfo& lf)
         inPin = true;
         continue;
       }
+      if (line.find("UNITS") != npos and line.find("DATABASE") == npos) {
+        inUnits = true;
+        continue;
+      }
     }
     if (inUnits && line.find("DATABASE") != npos) {
       ss >> str >> str >> str >> macroUnits;
@@ -308,6 +316,7 @@ void Netlist::loadLEF(const std::string& leffile, const DRC::LayerInfo& lf)
       }
     }
   }
+  COUT << "units : " << units << '\n';
   ifs.close();
 }
 
